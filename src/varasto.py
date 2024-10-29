2016dd77f680e4f94bdfbc8f1b248f38c87ad217
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2024-10-29 17:45:07 +0200

    varasto.py korjattu ja varasto_test.py:n test_ota_varastosta_negatiivinen_määrä_ei_muuta_saldoa testitapaus rikottu

--- src/varasto.py
+++ src/varasto.py
@@ -23,8 +23,8 @@ class Varasto:
     def lisaa_varastoon(self, maara):
         if maara < 0:
             return
-        if maara > self.paljonko_mahtuu():
-            self.saldo = self.saldo - maara
+        if maara <= self.paljonko_mahtuu():
+            self.saldo = self.saldo + maara
         else:
             self.saldo = self.tilavuus
 
