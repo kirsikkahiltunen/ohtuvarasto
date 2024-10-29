1a8a0cd1a4719b3426b5a53bb1a6d85b6f9f9dfd
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2024-10-29 17:16:42 +0200

    Unittestit lisätty, src/varasto.py tiedoston haarautumakattavuudeksi saatu 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,41 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_jos_tilavuus_negatiivinen_nollataan(self):
+        neg_tilavuus = Varasto(-1)
+
+        self.assertEqual(neg_tilavuus.tilavuus, 0.0)
+
+    def test_jos_saldo_negatiivinen_nollataan(self):
+        neg_saldo = Varasto(1, -1)
+
+        self.assertEqual(neg_saldo.saldo, 0.0)
+    
+    def test_lisaa_varastoon_ei_ylitä_kapasiteettia(self):
+        varasto = Varasto(10, 1)
+        varasto.lisaa_varastoon(20)
+
+        self.assertEqual(varasto.saldo, 10)
+
+    def test_lisaa_varastoon_negatiivinen_määrä_ei_muuta_saldoa(self):
+        varasto = Varasto(10, 1)
+        varasto.lisaa_varastoon(-5)
+
+        self.assertEqual(varasto.saldo, 1)
+    
+    def test_ota_varastosta_negatiivinen_määrä_ei_muuta_saldoa(self):
+        varasto = Varasto(10, 1)
+        varasto.ota_varastosta(-5)
+
+        self.assertEqual(varasto.saldo, 1)
+
+    def test_varastosta_ei_voi_ottaa_saldoa_enempää(self):
+        varasto = Varasto(10, 8)
+        varasto.ota_varastosta(9)
+
+        self.assertEqual(varasto.saldo, 0.0)
+
+    def test__str__tulostaa_oikein(self):
+        self.varasto.lisaa_varastoon(5)
+        self.assertEqual(self.varasto.__str__(), "saldo = 5, vielä tilaa 5")
